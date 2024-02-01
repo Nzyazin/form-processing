57071d9cea5a83feb9bc4f0b43d2e741f93a89ed
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-01 21:03:40 +0300

    frontend commit

--- main.js
+++ main.js
@@ -65,4 +65,81 @@ document.addEventListener('DOMContentLoaded', function () {
         rightAlign: false
     });
     priceMaskWithCurrency.mask(document.getElementsByName('price'));
+
+    // Отправка с формы
+    const form = {
+        name : $('input[type="name"]'),
+        mail : $('input[type="mail"]'),
+        phone : $('input[type="tel"]'),
+        price : $('input[type="price"]'),
+
+
+        initFirstEnterPage: function () {
+            let cookie = App.getCookie("firstEnterPage");
+            if (!cookie) {
+                let location = decodeURI(window.location.toString());
+                App.setCookie('firstEnterPage', location);
+            }
+            let referrerCookie = App.getCookie("firstReferrer");
+            if (!referrerCookie) {
+                let referrer = decodeURI(document.referrer ? document.referrer.toString() : "Прямой заход");
+                App.setCookie('firstReferrer', referrer, { expires: 3600 * 24 * 30 });
+            }
+        },
+
+        validateForm: function () {
+            $(this).validate({
+                rules: {
+                    phone: {
+                        required: true,
+                        minlength: 5,
+                        pattern: (placeNumber && placeNumber.length !== 0) ? `^\\+?${placeNumber} \\([\\d-() ]*` : '^\\+?7 \\(9[\\d-() ]*'
+                    }
+                },
+                messages: {
+                    phone: {
+                        pattern: 'Некорректный номер телефона.'
+                    },
+                }
+            });
+        },
+        sendAjax(e) {
+            e.preventDefault();
+            let $this = $(this);
+            if (!$this.valid()) return false;
+            let fd = new FormData(this);
+            let submit = $this.find('[type="submit"]');
+            submit.prop('disabled', true);           
+            
+            function logJqXhr(jqXHR) {
+                console.log(`status-text: ${jqXHR.statusText} \ncode: ${jqXHR.status} \nresponse: ${jqXHR.responseText} \n`, 'jqXHR is...');
+                console.log(jqXHR);
+            }
+
+            $.ajax({
+                url: '/send.php',
+                type: 'POST',
+                contentType: false,
+                processData: false,
+                data: fd,
+                async success(data, textStatus, jqXHR) {
+                    logJqXhr(jqXHR);
+                    let src = $this.attr('data-src');
+                    setTimeout(function () { window.location.pathname = src; }, 1000);
+                },
+                async error(jqXHR, textStatus, errorThrown) {
+                    logJqXhr(jqXHR);
+                    await Promise.allSettled(promisesSet);
+                    alert(jqXHR.responseText);
+                },
+                complete(jqXHR, textStatus) { submit.prop('disabled', false); }
+            })
+        }
+
+    };
+
+    if (form.main.length > 0) {
+        form.main.each(form.validateForm);
+        form.main.on('submit', form.sendAjax);
+    }
 });
