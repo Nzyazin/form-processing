245f419efca9ad96d85290f5afcf06aab4e45930
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-03 11:24:47 +0300

    Update amocrm.php

--- integration/amocrm.php
+++ integration/amocrm.php
@@ -1,1 +1,17 @@
-<!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Интеграция Amocrm</title><link type="image/x-icon" href="favicon.ico" rel="shortcut icon"><link href="https://statics.суперкухни.рф/styles.css?v=1706860079677" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Playfair+Display&amp;display=swap&amp;subset=cyrillic"></head><body><script class="amocrm_oauth" charset="utf-8" data-name="Росмебель квиз" data-description="Интеграция позволяет создавать сделки и контакты" data-redirect_uri="https://92.xn--e1agfmhheqeu.xn--p1ai/amocrm-oauth.php" data-secrets_uri="https://92.xn--e1agfmhheqeu.xn--p1ai/amocrm-secrets.php" data-scopes="crm" data-title="Установить интеграцию" data-compact="false" data-class-name="className" data-color="default" data-state="state" data-error-callback="functionName" data-mode="popup" src="https://www.amocrm.ru/auth/button.js"></script></body></html>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Интеграция Amocrm</title>
+    <link type="image/x-icon" href="favicon.ico" rel="shortcut icon">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display&amp;display=swap&amp;subset=cyrillic">
+</head>
+
+<body>
+    <script class="amocrm_oauth" charset="utf-8" data-name="Тест" data-description="Интеграция для сделок" data-redirect_uri="https://252.xn--e1agfmhheqeu.xn--p1ai/amocrm-oauth.php" data-secrets_uri="https://252.xn--e1agfmhheqeu.xn--p1ai/amocrm-secrets.php" data-scopes="crm" data-title="Установить интеграцию" data-compact="false" data-class-name="className" data-color="default" data-state="state" data-error-callback="functionName" data-mode="popup" src="https://www.amocrm.ru/auth/button.js"></script>
+</body>
+
+</html>
